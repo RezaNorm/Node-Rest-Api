6b8248ec8a27ac86f96ca44e396947efe5f41d28
ahmadreza narimisa <b.kooshan69@gmail.com>
2021-10-10 00:15:09 +0330

    add create post controller

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,4 +1,15 @@
-exports.getFeed = (req,res) => {
+exports.getPosts = (req, res) => {
+  res.status(200).json({
+    posts: [{ title: "first post", content: "this is the first post" }],
+  });
+};
 
-    
-}
+exports.createPost = (req, res) => {
+  const title = req.body.title;
+  const content = req.body.content;
+
+  res.status(201).json({
+    message: "post created successfully",
+    post: { id: new Date().toISOString(), title: title, content: content },
+  });
+};
