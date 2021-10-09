3ead3d18130e71f3c5ff14e47215ded9b3df6e29
ahmadreza narimisa <b.kooshan69@gmail.com>
2021-10-09 12:49:12 +0330

    server setup

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express")
+
+const app = express()
+
+const feedRoutes = require("./routes/feed")
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json())
+
+app.use("/feed",feedRoutes )
+
+app.listen(8080)
